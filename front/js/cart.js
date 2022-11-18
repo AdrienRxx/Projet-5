b7f252a651e2e94273ebf3f53ae327e3e3626ca3
AdrienRxx <adrien.roux82@gmail.com>
2022-11-18 12:06:10 +0100

    reconstruction du Dom de l'article panier

--- front/js/cart.js
+++ front/js/cart.js
@@ -42,32 +42,46 @@ if (!basket) {
       article.dataset.color = product.color
       cartItems.appendChild(article)
 
+      const div = document.createElement('div')
+      div.className = 'cart__item__img'
+      article.appendChild(div)
       const img = document.createElement('img')
-      img.className = 'cart__item__img'
       img.src = product.imageUrl
       img.alt = product.altTxt
-      article.appendChild(img)
+      div.appendChild(img)
+
+      const div1 = document.createElement('div')
+      div1.className = 'cart__item__content'
+      article.appendChild(div1)
+
+      const div2 = document.createElement('div')
+      div2.className = 'cart__item__content__description'
+      div1.appendChild(div2)
 
       const nameProduct = document.createElement('h2')
-      nameProduct.className = 'cart__item__content__description'
       nameProduct.innerText = product.name
-      cartItems.appendChild(nameProduct)
+      div2.appendChild(nameProduct)
 
 
       const colorProduct = document.createElement('p')
-      colorProduct.className = 'cart__item__content__description'
       colorProduct.innerText = 'Couleur: ' + product.color
-      cartItems.appendChild(colorProduct)
+      div2.appendChild(colorProduct)
 
       const priceProduct = document.createElement('p')
-      priceProduct.className = 'cart__item__content__description'
       priceProduct.innerText = product.price
-      cartItems.appendChild(priceProduct)
+      div2.appendChild(priceProduct)
+
+      const div3 = document.createElement('div')
+      div3.className = 'cart__item__content__settings'
+      div1.appendChild(div3)
+
+      const div4 = document.createElement('div')
+      div4.className = 'cart__item__content__settings__quantity'
+      div3.appendChild(div4)
 
       const settingsQuantity = document.createElement('p')
-      settingsQuantity.className = 'cart__item__content__settings__quantity'
       settingsQuantity.innerText = product.quantity
-      cartItems.appendChild(settingsQuantity)
+      div4.appendChild(settingsQuantity)
 
       const inputSettingsQuantity = document.createElement('input')
       inputSettingsQuantity.className = 'itemQuantity'
@@ -76,7 +90,7 @@ if (!basket) {
       inputSettingsQuantity.value = product.quantity
       inputSettingsQuantity.min = 1
       inputSettingsQuantity.max = 100
-      cartItems.appendChild(inputSettingsQuantity)
+      div4.appendChild(inputSettingsQuantity)
       let actualValue = inputSettingsQuantity.value
       inputSettingsQuantity.addEventListener("input", (e) => {
         const newValue = parseInt(e.target.value)
@@ -102,6 +116,10 @@ if (!basket) {
         }
       })
 
+      const div5 = document.createElement('div')
+      div5.className = 'cart__item__content__settings__delete'
+      div3.appendChild(div5)
+
       const contentSettingsDelete = document.createElement('p')
       contentSettingsDelete.className = 'deleteItem'
       contentSettingsDelete.innerText = 'Supprimer'
@@ -113,7 +131,7 @@ if (!basket) {
         updateNumberArticles();
 
       });
-      cartItems.appendChild(contentSettingsDelete)
+      div5.appendChild(contentSettingsDelete)
 
 
 
